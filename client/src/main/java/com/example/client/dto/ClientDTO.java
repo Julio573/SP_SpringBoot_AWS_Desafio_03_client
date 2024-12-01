4832862da22a18a64efa78d3b62fc5e46729bce3
Julio Peixoto <julio8497@hotmail.com>
2024-12-01 12:56:38 -0300

    Updating Client DTO

--- client/src/main/java/com/example/client/dto/ClientDTO.java
+++ client/src/main/java/com/example/client/dto/ClientDTO.java
@@ -1,6 +1,8 @@
 package com.example.client.dto;
 
+import jakarta.validation.constraints.Email;
 import lombok.*;
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
 
 @Getter
 @Setter
@@ -9,6 +11,10 @@ import lombok.*;
 public class ClientDTO {
 
     private Long id;
+
+    @NotBlank
     private String name;
+
+    @Email(message = "Invalid email format")
     private String email;
 }
