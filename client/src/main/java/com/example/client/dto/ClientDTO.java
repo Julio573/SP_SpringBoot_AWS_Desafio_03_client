c5fc5665620a8cdb584a3ca627d8e3bba884acdf
Julio Peixoto <julio8497@hotmail.com>
2024-12-01 10:03:52 -0300

    Adding ClientDTO

---
+++ client/src/main/java/com/example/client/dto/ClientDTO.java
@@ -0,0 +1,14 @@
+package com.example.client.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+}
